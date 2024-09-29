fec19b813283e3767065e83b8b13fab4631597f4
Diego Schmidt <schmas+gh@pm.me>
2024-09-29 19:15:41 -0300

    feat: add LLM GPT call function
    - Add ai_task_request function to call GPT service
    - Integrate call_gpt and PrintCommand usage
    - Include test for ai_task_request function

--- auto_gippity/src/helpers/general.rs
+++ auto_gippity/src/helpers/general.rs
@@ -1,3 +1,5 @@
+use crate::apis::call_request::call_gpt;
+use crate::helpers::command_line::PrintCommand;
 use crate::models::general::llm::Message;
 
 // Extend ai function to encourage certain specific output
@@ -20,6 +22,31 @@ pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -
     }
 }
 
+// Perform call to LLM GPT
+pub async fn ai_task_request(
+    msg_context: String,
+    agent_position: &str,
+    agent_operation: &str,
+    function_pass: for<'a> fn(&'a str) -> &'static str,
+) -> String {
+    // Extend AI function
+    let extended_msg = extend_ai_function(function_pass, &msg_context);
+
+    // Print current status
+    PrintCommand::AICall.print_agent_message(agent_position, agent_operation);
+
+    // get LLM response
+    let llm_response = call_gpt(vec![extended_msg.clone()]).await;
+
+    // Handle success
+    match llm_response {
+        Ok(res) => res,
+        Err(_) => call_gpt(vec![extended_msg])
+            .await
+            .expect("Failed twice to call OpenAI"),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -29,5 +56,25 @@ mod tests {
     fn tests_extending_ai_function() {
         let message = extend_ai_function(convert_user_input_to_goal, "dummy variable");
         assert_eq!(message.role, "system");
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn tests_ai_task_request() {
+        let ai_func_param = "Build me a webserver for making stock price api request".to_string();
+        let agent_position = "Managing Agent";
+        let agent_operation = "Defining use requirement";
+
+        let result = ai_task_request(
+            ai_func_param,
+            agent_position,
+            agent_operation,
+            convert_user_input_to_goal,
+        )
+        .await;
+
+        dbg!(&result);
+
+        assert!(result.len() > 0);
     }
 }
